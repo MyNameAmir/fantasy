c574e8fd706c4bfe900a668a7ab97c4096795eb1
unknown <amiralimoin>
2020-12-16 23:52:15 -0800

    major developments in the project

--- src/Fantasy.ts
+++ src/Fantasy.ts
@@ -1,10 +1,34 @@
 import axios from 'axios'
+import {fetchBootstrap} from 'fpl-api'
+
 
 const URL = 'https://fantasy.premierleague.com/api/entry/565066/'
 
 
-export async function getAllData(): Promise<object>{
-    let resp = await axios.get(URL);
-    console.log("resp: ", resp.config.data)
-    return resp.data;
-} 
+export async function getAllData(): Promise<String>{
+    let resp = await fetchBootstrap();
+    console.log("resp: ", resp.elements[0].first_name);
+    return resp.elements[0].first_name;
+}   
+
+export async function getCertainData(id: number): Promise<String>{
+    let resp = await fetchBootstrap();
+    console.log("resp: ", resp.elements[0].first_name);
+    return resp.elements[id].first_name;
+}   
+
+export async function getPlayerData(name: String): Promise<String>{
+    let resp = await fetchBootstrap();
+    let result: String;
+    result = "";
+    for(let i of resp.elements){
+        if(i.first_name.toLowerCase() == name){
+            if(result == "")
+                result += `Retrieving form for all players with the name '${name}':\n`
+            result += `${name} ${i.second_name} has form of ${i.form}\n`;
+        }
+    }
+    if (result != "")
+        return result;
+    return "player form not available";
+}   
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
 import express from 'express';
 import {getAllData} from './Fantasy'
+import {getCertainData} from './Fantasy'
+import {getPlayerData} from './Fantasy'
 
 const app = express();
+let data: String;
+let num: number;
 
-app.get('/', (req: any, res: any)=>{
-    res.send(getAllData());
+getAllData().then((resp) => data = resp);
+
+let getRandomColor = (): number => {
+    return Math.floor(Math.random() * 256)
+}
+
+app.get('/', async (req: any, res: any)=>{
+    data = await getAllData();
+    res.send(data);
+})
+
+app.get(`/id/:id`, async (req: any, res: any)=>{
+    let style = {
+        color: `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`
+    }
+    let id = req.params.id as number;
+    res.send(`<p style=color:${style.color}>getting the data for player ${id}: ${await getCertainData(id)}</p>`);
+})
+
+app.get('/name/:name', async(req: any, res: any) => {
+    let style = {
+        color: `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`
+    }
+    let name = req.params.name.toLowerCase();
+    res.send(`<p style=color:${style.color}>${await getPlayerData(name)}</p>`);
 })
 
 const server = app.listen(process.env.PORT || 8080);
 server.on('listening', () => {
-    getAllData().then(console.log).catch(console.error);
+    console.log("Hi there ", data);
 });
